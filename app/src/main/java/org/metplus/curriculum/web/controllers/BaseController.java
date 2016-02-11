94912638b25f908f6ef43a596635124324f0c6cd
Joao Pereira <joaopapereira@gmail.com>
2016-02-10 19:38:40 -0500

    First import

--- app/src/main/java/org/metplus/curriculum/web/controllers/BaseController.java
+++
@@ -1,8 +0,0 @@
-package org.metplus.curriculum.web.controllers;
-
-/**
- * Created by Joao Pereira on 14/11/2015.
- */
-public class BaseController {
-    final static public String baseUrl = "/api/v1/";
-}
